b3545eb618f91be73f38ff657c5f88ac6d92f9b0
Stephan Raab <sarcartist@gmail.com>
2024-12-16 12:46:29 -0500

    simplification

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
 
     return(
         <>
-            <p onClick={() => setIsClosed(!isClosed)} className='close'>&times;</p>
+            <button onClick={() => setIsClosed(!isClosed)} className='close'>&times;</button>
             {!isClosed && (
                 <div className="steps">
                     <div className="numbers">

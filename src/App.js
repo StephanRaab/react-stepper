a09cc2fa2f88ac641d1b492fd9174b262a2e9126
Stephan Raab <sarcartist@gmail.com>
2024-12-16 11:37:11 -0500

    start from first index

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const messages = [
 ];
 
 export default function App() {
-    const [step, setStep] = useState(0);
+    const [step, setStep] = useState(1);
     const [close, setClose] = useState(false);
 
     function toggleSteps() {
@@ -15,13 +15,13 @@ export default function App() {
     }
 
     function incrementStep() {
-        if (step !== 2) {
+        if (step !== 3) {
             setStep(step + 1);
         }
     }
 
     function decrementStep() {
-        if (step !== 0) {
+        if (step !== 1) {
             setStep(step - 1);
         }
     }
@@ -32,17 +32,17 @@ export default function App() {
             {!close &&(
                 <div className="steps">
                     <div className="numbers">
-                        <div className={`${step >= 0 ? 'active' : ''}`}>1</div>
-                        <div className={`${step >= 1 ? 'active' : ''}`}>2</div>
-                        <div className={`${step >= 2 ? 'active' : ''}`}>3</div>
+                        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
                     </div>
 
-                    <p className="message">Step {step + 1}: {messages[step]}</p>
+                    <p className="message">Step {step}: {messages[step-1]}</p>
 
                     <div className="buttons">
-                        <button onClick={decrementStep} className={`${step === 0 ? 'disabled' : 'active'}`}>Previous
+                        <button onClick={decrementStep} className={`${step === 1 ? 'disabled' : 'active'}`}>Previous
                         </button>
-                        <button onClick={incrementStep} className={`${step === 2 ? 'disabled' : 'active'}`}>Next
+                        <button onClick={incrementStep} className={`${step === 3 ? 'disabled' : 'active'}`}>Next
                         </button>
                     </div>
                 </div>
